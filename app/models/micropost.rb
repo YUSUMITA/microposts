cef5bee0b3ee745aae5f498d5d96834840afe964
yu.sumita <yutennis1315@gmail.com>
2020-10-22 00:39:56 +0000

    added dependent destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,6 +4,6 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: {maximum: 255}
   
     
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :favoritedusers, through: :favorites, source: :user
 end
